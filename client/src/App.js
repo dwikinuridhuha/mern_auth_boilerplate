cf05eec772daa739788ee5c0a4f98d038461e7d8
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2020-12-25 22:43:00 +0700

    server is done, now client in progres

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Header } from './componets/Header';
+import { Balance } from './componets/Balance';
+import { IncomeExpenses } from './componets/IncomeExpenses';
+import { TransactionList } from './componets/TransactionList';
+import { AddTransaction } from './componets/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({ path: './config/.env' });
+
+// set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+   res.send("oke");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+   console.log(`running on ${PORT}`);
+});
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STR, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+}, (err) => {
+    if(err) throw err;
+    console.log("mongoDB connection established");
+});
+
+app.use('/users', require('./routes/userRoutes'));
